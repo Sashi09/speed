c91ecaefb372bd825fe8c7c43a0c35fddc993a33
Sashi09 <katakamsashipoornasivani@gmail.com>
2026-01-15 11:09:52 +0530

    Delete assets/css/quotes.js

--- assets/css/quotes.js
+++
@@ -1,7 +0,0 @@
-const TYPING_QUOTES = [
-    "Programming is the art of algorithm design and the craft of debugging code.",
-    "The best error message is the one that never shows up.",
-    "Web development is a constant journey of learning new technologies.",
-    "Success is not final, failure is not fatal: it is the courage to continue.",
-    "Write code as if the person who ends up maintaining it is a psychopath."
-];
